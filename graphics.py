f27e389332daa1606de33a1318e21ad52a3356b7
Vedant Patil <vedant.waman.patil@gmail.com>
2025-03-27 05:45:17 -0400

    Able to draw lines

--- graphics.py
+++ graphics.py
@@ -1,6 +1,42 @@
 from tkinter import Tk, BOTH, Canvas
 
 
+class Point:
+    # x=0 is the left of the screen
+    # y=0 is the top of the screen
+    def __init__(self, x: int, y: int) -> None:
+        self.__x = x
+        self.__y = y
+
+    def getX(self) -> int:
+        return self.__x
+
+    def getY(self) -> int:
+        return self.__y
+
+
+class Line:
+    def __init__(self, point1: Point, point2: Point) -> None:
+        self.__point1 = point1
+        self.__point2 = point2
+
+    def getPointOne(self) -> Point:
+        return self.__point1
+
+    def getPointTwo(self) -> Point:
+        return self.__point2
+
+    def draw(self, canvas: Canvas, fill_color: str):
+        canvas.create_line(
+            self.getPointOne().getX(),
+            self.getPointOne().getY(),
+            self.getPointTwo().getX(),
+            self.getPointTwo().getY(),
+            fill=fill_color,
+            width=2,
+        )
+
+
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
@@ -9,6 +45,12 @@ class Window:
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def draw_line(self, line: Line, fill_color: str):
+        line.draw(self.getCanvas(), fill_color)
+
+    def getCanvas(self) -> Canvas:
+        return self.__canvas
 
     def redraw(self):
         self.__root.update_idletasks()
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from graphics import Line
+from graphics import Point
 from graphics import Window
 
 
 def main():
     win = Window(800, 600)
+    for i in range(10, 200, 10):
+        win.draw_line(Line(Point(i + 10, i - 5), Point(i - 10, i + 10)), "black")
+
     win.wait_for_close()
 
 
